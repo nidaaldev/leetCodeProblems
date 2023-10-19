46110ba6af8f41202945b8205029507f09c63b9d
NEONz <57320046+nidaaldev@users.noreply.github.com>
2023-10-19 13:26:07 +0200

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+
+    let result = [];
+
+     for (let i = 0; i <= nums.length - 1; i++) {
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
+    }
+  }
+
+  return result;
+};
+
+const numbers = [2, 11, 3, 7];
+
+twoSum(numbers, 9);
