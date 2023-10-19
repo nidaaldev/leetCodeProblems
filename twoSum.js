3307e6161f814a968a30b5d7c4fb52eaafaf129e
NEONz <57320046+nidaaldev@users.noreply.github.com>
2023-10-19 13:26:52 +0200

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -7,15 +7,15 @@ const twoSum = (nums, target) => {
 
     let result = [];
 
-     for (let i = 0; i <= nums.length - 1; i++) {
-    for (let j = i + 1; j <= nums.length - 1; j++) {
-      if (nums[i] + nums[j] === target) {
-        result.push(i, j);
-      }
+    for (let i = 0; i <= nums.length - 1; i++) {
+        for (let j = i + 1; j <= nums.length - 1; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i, j);
+            }
+        }
     }
-  }
 
-  return result;
+    return result;
 };
 
 const numbers = [2, 11, 3, 7];
